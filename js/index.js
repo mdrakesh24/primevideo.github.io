9cc1da69817e7d17359b5e22f4e2c127d3bd7ced
Md Rakesh Ali <mdrakeshali2412@gmail.com>
2024-02-25 01:05:28 +0530

    Initial Commit of the Project

---
+++ js/index.js
@@ -0,0 +1,37 @@
+//Sign In Modal
+var modal = document.getElementById('signInModal');
+var btn = document.getElementById("signInBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//Slider Swiper.js
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
